3b3b6b9c52b3a4359e25757382aad438385c2e01
Flavien Bouleau <flavien.bouleau@epitech.eu>
2023-07-03 20:11:45 +0200

    :sparkles: create the pygame window

--- game.py
+++ game.py
@@ -1,9 +1,36 @@
 import pygame
 
+
 class Game:
+    """General class for the game
+    """
 
     def __init__(self):
-        pass
+        """Create the pygame window and initialize all the elements of the game
+        """
+
+        pygame.init()
+        pygame.display.set_mode((1080, 720))
+        pygame.display.set_icon(pygame.image.load('assets/images/icon.png'))
+        pygame.display.set_caption('Battleship')
+
+        self.running = False
 
     def run(self):
-        pass
+        """Run the game
+        """
+
+        self.running = True
+
+        while self.running:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+        
+        self.close()
+
+    def close(self):
+        """Close the game
+        """
+        pygame.quit()
--- main.py
+++ main.py
@@ -2,5 +2,10 @@ from game import Game
 
 
 def main():
+	"""Start the game
+	"""
 	game = Game()
 	game.run()
+
+if __name__ == '__main__':
+	main()
