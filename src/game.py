dfc5bcf223fa5a62bf04e2c8b6662cb0fe764eeb
Adyarel <138512882+Adyarel@users.noreply.github.com>
2023-07-03 23:45:18 +0200

    :sparkles: Create Map generation

--- src/game.py
+++ src/game.py
@@ -1,4 +1,7 @@
+import time
+
 import pygame
+from src import terrainGen
 
 
 class Game:
@@ -9,8 +12,23 @@ class Game:
         """Create the pygame window and initialize all the elements of the game
         """
 
+        self.screen_size = (1080, 720)
+        self.sealevel = 145
+
+        # calcul du temps de création de la carte
+        t_debut = time.time()
+        # création de la carte (heightmap)
+        self.map_data = terrainGen.GenerateMap(self.screen_size)
+        # création de l'image associée à la carte
+        self.map_image = pygame.surfarray.make_surface(
+            terrainGen.ColorMap(self.map_data)
+            .get_color_map_array(self.sealevel))
+        t_fin = time.time()
+
+        print("Map générée en " + str(int((t_fin-t_debut)*1000)) + " ms")
+
         pygame.init()
-        pygame.display.set_mode((1080, 720))
+        self.screen = pygame.display.set_mode(self.screen_size)
         pygame.display.set_icon(pygame.image.load('assets/images/icon.png'))
         pygame.display.set_caption('Battleship')
 
@@ -24,10 +42,16 @@ class Game:
 
         while self.running:
 
+            # afficher qqchose à l'écran
+            self.screen.blit(self.map_image, (0, 0))
+
+            # mettre à jour l'écran
+            pygame.display.flip()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
-        
+
         self.close()
 
     def close(self):
---
+++ src/terrainGen.py
@@ -0,0 +1,66 @@
+import random
+import noise
+import numpy
+
+
+class GenerateMap:
+    """Génération procédurale du terrain """
+
+    def __init__(self, size=(50, 50), scale=500, octaves=4, persistence=0.6, lacunarity=2.0,
+                 x_starting_pos=0, y_starting_pos=0):
+        """scale : niveau de zoom, 100 dézoomé, 500 zoomé
+           octave : lissage fin de la carte, 4 très lisse, 10 dentelé
+           persistence :
+           lacunarity : lissage large de la carte, 1 très lisse, 3 dentellé
+        """
+        self.scale = scale
+        self.octaves = octaves
+        self.persistence = persistence
+        self.lacunarity = lacunarity
+
+        self.x_starting_pos = x_starting_pos
+        self.y_starting_pos = y_starting_pos
+
+        self.mapSize = size  # size in pixels
+        self.heightMap = numpy.zeros(self.mapSize)
+
+        self.generate_map()
+
+    def generate_map(self):
+        """Génération de la heightmap par bruit de perlin"""
+        random_nr = random.randint(0, self.mapSize[0])
+        for i in range(self.mapSize[0]):
+            for j in range(self.mapSize[1]):
+                new_i = i + self.y_starting_pos
+                new_j = j + self.x_starting_pos
+
+                self.heightMap[i][j] = noise.pnoise3(new_i / self.scale, new_j / self.scale, random_nr,
+                                                     octaves=self.octaves,
+                                                     persistence=self.persistence, lacunarity=self.lacunarity,
+                                                     repeatx=10000000, repeaty=10000000, base=0)
+                self.heightMap[i][j] = int((self.heightMap[i][j] + 1) * 128)
+        return self.heightMap
+
+
+class ColorMap:
+    """Transformation de la heightmap en image RGB"""
+
+    sea = [65, 105, 225]
+    sand = [210, 180, 140]
+    grass = [34, 139, 34]
+
+    def __init__(self, generate_map: GenerateMap):
+        self.map = generate_map
+        self.colored_map = numpy.zeros((self.map.mapSize[0], self.map.mapSize[1], 3))
+
+    def get_color_map_array(self, sea_level):
+        """La couleur est définie en fonction de la valeur de la heightmap et du sea level"""
+        for i in range(self.map.mapSize[0]):
+            for j in range(self.map.mapSize[1]):
+                if self.map.heightMap[i][j] < sea_level:
+                    self.colored_map[i][j] = ColorMap.sea
+                elif self.map.heightMap[i][j] < (sea_level + 5):
+                    self.colored_map[i][j] = ColorMap.sand
+                else:
+                    self.colored_map[i][j] = ColorMap.grass
+        return self.colored_map
