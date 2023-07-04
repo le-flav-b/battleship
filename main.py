b66791fee444d7facae64a37ac5bf17f5720948e
Flavien Bouleau <flavien.bouleau@epitech.eu>
2023-07-04 16:53:41 +0200

    :art: create 3 state (menu, game, game over) and reform the code properly

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from src.game import Game
+from sys import argv
 
 
-def main():
+def main(window_width: int= 1080, window_height: int= 720):
     """Start the game
     """
-    game = Game()
+    game = Game(window_width, window_height)
     game.run()
 
 
 if __name__ == '__main__':
-    main()
+
+    if len(argv) == 3:
+        main(int(argv[1]), int(argv[2]))
+    else:
+        main()
