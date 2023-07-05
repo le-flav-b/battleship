219588478afb6b794d465bb9fe3c2484397d7d88
Adyarel <138512882+Adyarel@users.noreply.github.com>
2023-07-05 16:16:25 +0200

    :pencil2: correction of spelling mistakes

--- src/player.py
+++ src/player.py
@@ -5,7 +5,7 @@ from src.physics import Speed, Pos
 
 class Player(Boat):
 
-    boat_1_image = pygame.image.load("assets/images/boat_ennemy.png")
+    boat_1_image = pygame.image.load("assets/images/boat_enemy.png")
     boat_2_image = pygame.image.load("assets/images/boat_friendly.png")
 
     def __init__(self, pos: Pos, speed: Speed, mass, max_power, image):
